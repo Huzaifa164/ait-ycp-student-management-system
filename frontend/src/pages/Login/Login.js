2966f94b8dd1fe16242ecd001577f8eac0ec4362
Huzaifa164 <ansarihuzaifa654321@gmail.com>
2023-08-21 23:38:07 +0530

    Initial Commit

---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Login.css";
+import Footer from "../../components/Footer/Footer";
+
+const Login = () => {
+  return (
+    <>
+      <section className="vh-100 login">
+        <h1 className="heading">YCP DATABASE MANAGEMENT</h1>
+        <div className="container-fluid h-custom">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-md-9 col-lg-6 col-xl-5">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                className="img-fluid"
+                alt="Sample image"
+              />
+            </div>
+            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+              <form className="login-form">
+                <img src="./images/ycp-logo.jpeg" className="logo mb-5" />
+                <div className="form-outline mb-4">
+                  <input
+                    type="text"
+                    id="form3Example3"
+                    className="form-control form-control-lg"
+                    placeholder="Enter a valid user Id"
+                    required
+                  />
+                </div>
+                <div className="form-outline mb-3">
+                  <input
+                    type="password"
+                    id="form3Example4"
+                    className="form-control form-control-lg"
+                    placeholder="Enter password"
+                    required
+                  />
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="form-check mb-0">
+                    <input
+                      className="form-check-input me-2"
+                      type="checkbox"
+                      defaultValue
+                      id="form2Example3"
+                    />
+                    <label className="form-check-label" htmlFor="form2Example3">
+                      Remember me
+                    </label>
+                  </div>
+                  <a href="#!" className="text-body">
+                    Forgot password?
+                  </a>
+                </div>
+                <div className="text-center text-lg-start mt-4 pt-2">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg"
+                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
+  );
+};
+
+export default Login;
